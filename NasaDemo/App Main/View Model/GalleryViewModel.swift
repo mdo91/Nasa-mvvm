089575b8a8953265e36d2636dfcf0e1dd069bd08
mdo91 <hesoka_mxx@yahoo.com>
2020-09-29 11:02:52 +0300

    adding viewModel for the main view controller

---
+++ NasaDemo/App Main/View Model/GalleryViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  GalleryViewModel.swift
+//  NasaDemo
+//
+//  Created by Mdo on 29/09/2020.
+//  Copyright © 2020 Mdo. All rights reserved.
+//
+
+import Foundation
+
+
+class GalleryViewModel{
+    
+    
+    
+}
